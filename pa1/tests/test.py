cd9eaea02cbed5974baa05db0a816ee0b7509714
Jack <jack.huang78@gmail.com>
2013-04-16 14:33:52 -0700

    clean up trackedfiles

--- pa1/tests/test.py
+++ pa1/tests/test.py
@@ -9,7 +9,7 @@ for arg in sys.argv[1:]:
     print "COOL file: " + arg
     os.system("rm -f lexer.out mylexer.out")
     os.system("/usr/class/cs143/bin/coolc -k " + arg + " &> /dev/null")
-    os.system("./lexer " + arg + " > mylexer.out")
+    os.system("../lexer " + arg + " > mylexer.out")
     res = os.system("diff lexer.out mylexer.out")
     if res == 0:
         print "PASS"
