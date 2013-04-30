5bd04061d53f6307afc5e39d307db108eac58440
Jack <jack.huang78@gmail.com>
2013-04-29 19:46:25 -0700

    remove 'parse error'

--- pa2/test/test.py
+++ pa2/test/test.py
@@ -8,7 +8,7 @@ for arg in sys.argv[1:]:
 	# initialization
 	print 'COOL file: ' + arg
 	os.system('rm -f parser.out myparser.out parser_noline.out myparser_noline.out')
-	os.system('rm -f parser.err myparser.err')
+	os.system('rm -f parser.err myparser.err parser_noline.err myparser.noline.err')
 	
 	# run our implementation and the reference 
 	print 'running reference...' 
@@ -23,10 +23,19 @@ for arg in sys.argv[1:]:
 	with open('myparser.out', 'r') as fin, open('myparser_noline.out', 'w') as fout: 
 		for line in fin:
 			fout.write(re.sub('\s*#[0-9]+\n', '', line))
+			
+	# remove line numbers from parser.err and myparser.err
+	with open('myparser.err', 'r') as fin, open('myparser_noline.err', 'w') as fout: 
+		for line in fin:
+			fout.write(re.sub('parse error at or near', 'syntax error at or near', line))	
+			
+	with open('parser.err', 'r') as fin, open('parser_noline.err', 'w') as fout: 
+		for line in fin:
+			fout.write(line)
 		
 	# compare results (out and err)
 	res1 = os.system('diff parser_noline.out myparser_noline.out')
-	res2 = os.system('diff parser.err myparser.err')
+	res2 = os.system('diff parser_noline.err myparser_noline.err')
 	if res1 != 0:
 		print 'FAIL out'
 	else:
