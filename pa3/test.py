85bf330fcfd7158c10117bc3c1280a16972e10e0
Jack <jack.huang78@gmail.com>
2013-05-09 18:09:28 -0700

    modify test script

---
+++ pa3/test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+import os, sys, re
+
+print '\n>>> CS143 PA3 Testing Script <<<'
+
+print 'Building semant...'
+os.system('gmake semant')
+
+for arg in sys.argv[1:]:
+
+	print 'Input file: ' + arg
+
+	# run our implementation and the reference 
+	print 'Running reference...' 
+	os.system('/usr/class/cs143/bin/.coolref/lexer ' + arg + 
+		' | /usr/class/cs143/bin/.coolref/parser'
+		' | /usr/class/cs143/bin/.coolref/semant > semant.out 2> semant.err')
+	
+	print 'Running our implementation...'
+	#os.system('../mysemant ' + arg + ' > mysemant.out 2> mysemant.err')
+	os.system('./lexer ' + arg + 
+		' | ./parser ' + arg + 
+		' | ./semant ' + arg)
+	
+	exit(0)
+	
+
+	# compare outputs
+	print 'Compare output...'
+	if os.system('diff semant.out mysemant.out') == 0:
+		print 'PASS'
+	else:
+		print 'FAIL'
+	
+	print 'Compare error...'
+	if os.system('diff semant.err mysemant.err') == 0:
+		print 'PASS'
+	else:
+		print 'FAIL'
+	
+
