4d5a0e59f2fa539c48142cf137edbf584ad2364a
bwang29 <bwang29@gmail.com>
2013-05-30 21:01:10 -0700

    GC

--- pa4/cool-tree.java
+++ pa4/cool-tree.java
@@ -680,7 +680,7 @@ class assign extends Expression {
         Variable x = (Variable)CgenClassTable.ct.lookup(name);
         // emit assign plus garbage collection jal
         x.emitStore(s);
-        CgenSupport.emitAddiu(CgenSupport.A1,CgenSupport.SELF,x.getOffset(),s);
+        CgenSupport.emitAddiu(CgenSupport.A1,CgenSupport.SELF,4 * x.getOffset(),s);
         CgenSupport.emitGCAssign(s);
         s.println("# end of assign to " + name);
 	}
