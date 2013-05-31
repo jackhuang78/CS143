bec73571c72d92b74e44e4ad9c0231e888c1424f
bwang29 <bwang29@gmail.com>
2013-05-30 21:20:18 -0700

    GC

--- pa4/cool-tree.java
+++ pa4/cool-tree.java
@@ -30,6 +30,8 @@ class FieldVar extends Variable{
     }
     public void emitStore(PrintStream s) {
         CgenSupport.emitStore(CgenSupport.ACC, -offset, CgenSupport.FP, s);
+        CgenSupport.emitAddiu(CgenSupport.A1,CgenSupport.SELF,4*offset,s);
+        CgenSupport.emitGCAssign(s);
     }
 }
 
